7709d7c91fe47abdb23d1f2da5e346558b6e9900
Jonathan Adeline <jonathan@baserow.io>
2026-02-27 15:47:52 +0400

    please linter

--- web-frontend/modules/database/components/view/grid/fields/GridViewFieldLinkRow.vue
+++ web-frontend/modules/database/components/view/grid/fields/GridViewFieldLinkRow.vue
@@ -185,8 +185,7 @@ export default {
       }
 
       const selectModal = this.$refs.selectModal.$refs.modal
-      const rowEditModal =
-        this.$refs.rowEditModal.$refs.modal.$refs.modal
+      const rowEditModal = this.$refs.rowEditModal.$refs.modal.$refs.modal
 
       const openModals = [
         ...this.collectModalElements(selectModal),
@@ -196,8 +195,7 @@ export default {
       return (
         !openModals.some((modal) => {
           return isElement(modal, event.target)
-        }) &&
-        document.body.contains(event.target)
+        }) && document.body.contains(event.target)
       )
     },
     /**
